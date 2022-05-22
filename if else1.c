3b19563670aec2658e5ece0cad5ad82cbaab2f6c
Athoi1040 <105834245+Athoi1040@users.noreply.github.com>
2022-05-22 11:34:51 +0600

    Add files via upload

---
+++ if else1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("Ienter the value of a=");
+scanf("%d",&a);
+printf("inter the value of b=");
+scanf("%d",&b);
+
+if(a>b){
+
+printf("a is bigger ");
+
+}
+
+else{
+ printf("b is bigger") ;
+
+ return 0;
+
+}
+
+
+}
