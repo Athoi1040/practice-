930d6f6a2cace1d621d27079a882a7447d296de8
Athoi1040 <105834245+Athoi1040@users.noreply.github.com>
2022-08-11 22:22:11 +0600

    Add files via upload

---
+++ boot camp 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,sum;
+printf("Enter three angle ");
+scanf("%d\n %d\n %d\n",&a,&b,&c);
+sum=a+b+c;
+if(sum==180)
+{
+    printf("the triangle is valid");
+}
+
+else{
+printf("the triangle is not valid");
+}
+return 0;
+}
